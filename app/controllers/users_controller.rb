ad6426e807b431500aecd824b17ed4b20ea043c9
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-02 20:17:17 +0000

    Almost there! There's a problem in venues, but comments and users are done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    username = params.fetch("query_username")
+    username = params.fetch("user_id")
     matching_users = User.where({ :username => username })
     @user = matching_users.at(0)
 
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     
     the_user.username = params.fetch("query_username")
     the_user.save
-    redirect_to("/users/#{user.username}")
+    redirect_to("/users/#{the_user.username}")
   end
 
 end
